23ecc46c81a53c6544ba9b94dee48b58ea0d11da
Brittany Rausch <berausch247@gmail.com>
2016-07-01 09:50:40 -0700

    Add the finished cshtml contact form, place holder htmls or cshtmls, and a partially constructed Homemodule

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,26 +10,47 @@ namespace ToDoList
     {
       Get["/"] = _ => {
         return View["index.cshtml"];
-      };
-      Get["/tasks"] = _ => {
-        List<Task> allTasks = Task.GetAll();
-        return View["tasks.cshtml", allTasks];
-      };
-      Get["/tasks/new"] = _ => {
-        return View["task_form.cshtml"];
-      };
-      Post["/tasks"] = _ => {
-        Task newTask = new Task(Request.Form["new-task"]);
-        List<Task> allTasks = Task.GetAll();
-        return View["tasks.cshtml", allTasks];
-      };
-      Post["/tasks_cleared"] = _ => {
-        Task.ClearAll();
-        return View["tasks_cleared.cshtml"];
-      };
-      Get["/tasks/{id}"] = parameters => {
-        Task task = Task.Find(parameters.id);
-        return View["/task.cshtml", task];
+      }
+      Get["/add_contact"] = _ => {
+        return View["contact_form.cshtml"];
+      }
+      Get["/contacts"] = _ => {
+        List<Contact> allContacts = Contact.GetAll();
+        return View["contacts.cshtml"];
+      }
+      Post["/contact_created"] = _ => {
+        Contact newContact = new Contact(
+          Request.Form["new-name"],
+          Request.Form["new-phone"],
+          Request.Form["new-address1"],
+          Request.Form["new-address2"]
+        );
+        return View["contact_created.cshtml"];
+      }
+
+
+      // Get["/"] = _ => {
+      //   return View["index.cshtml"];
+      // };
+      // Get["/tasks"] = _ => {
+      //   List<Task> allTasks = Task.GetAll();
+      //   return View["tasks.cshtml", allTasks];
+      // };
+      // Get["/tasks/new"] = _ => {
+      //   return View["task_form.cshtml"];
+      // };
+      // Post["/tasks"] = _ => {
+      //   Task newTask = new Task(Request.Form["new-task"]);
+      //   List<Task> allTasks = Task.GetAll();
+      //   return View["tasks.cshtml", allTasks];
+      // };
+      // Post["/tasks_cleared"] = _ => {
+      //   Task.ClearAll();
+      //   return View["tasks_cleared.cshtml"];
+      // };
+      // Get["/tasks/{id}"] = parameters => {
+      //   Task task = Task.Find(parameters.id);
+      //   return View["/task.cshtml", task];
       };
     }
   }
